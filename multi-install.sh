eb782c2bcbf23fa94d0d5741b926392ed4b04b19
RealKasumi <49617028+RealKasumi@users.noreply.github.com>
2022-03-08 16:36:06 +0000

    yt-dlp is now used

--- multi-install.sh
+++ multi-install.sh
@@ -8,11 +8,11 @@ else
     echo
 fi
 
-echo "youtube-dl installer for Termux-Android"
+echo "yt-dlp installer for Termux-Android"
 echo
 echo "Select an option:"
-echo "1: Install youtube-dl"
-echo "2: Uninstall youtube-dl"
+echo "1: Install yt-dlp"
+echo "2: Uninstall yt-dlp"
 echo "Any other key: quit"
 echo
 echo -n "Select any option and hit ENTER: "
@@ -23,19 +23,20 @@ then
     echo 
     echo "The following processes are about to occur:"
     echo "Install/Update dependency: Python (via apt)"
-    echo "youtube-dl is downloaded and installed to /data/data/com.termux/files/usr/bin"
-    echo "youtube-dl is made executable"
+    echo "Install/Update dependency: ffmpeg (via apt)"
+    echo "yt-dlp is downloaded and installed to /data/data/com.termux/files/usr/bin"
+    echo "yt-dlp is made executable"
     echo
     echo -n 'To continue, enter "y". To abort, press any other key: '
     read y
     if [[ $y == "y" ]] 
     then
-        echo "Installing/updating Python (using apt)"
-        apt -y install python
-        echo "Installing youtube-dl to /data/data/com.termux/files/usr/bin/youtube-dl (using curl)"
-        curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /data/data/com.termux/files/usr/bin/youtube-dl
-        echo "Making youtube-dl executable (using chmod)"
-        chmod a+rx /data/data/com.termux/files/usr/bin/youtube-dl
+        echo "Installing/updating dependencies (using apt)"
+        apt -y install python ffmpeg
+        echo "Installing yt-dlp to /data/data/com.termux/files/usr/bin/yt-dlp (using curl)"
+        curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /data/data/com.termux/files/usr/bin/yt-dlp
+        echo "Making yt-dlp executable (using chmod)"
+        chmod a+rx /data/data/com.termux/files/usr/bin/yt-dlp
         echo "Install complete."
         exit
     else 
@@ -46,16 +47,16 @@ elif [[ $x -eq 2 ]]
 then
     echo 
     echo "The following processes are about to occur:"
-    echo "youtube-dl is removed from /data/data/com.termux/files/usr/bin"
+    echo "yt-dlp is removed from /data/data/com.termux/files/usr/bin"
     echo
     echo -n 'To continue, enter "y". To abort, press any other key: '
     read y
     if [[ $y == "y" ]] 
     then
-        echo "Removing youtube-dl (using rm)"
-        rm -rf /data/data/com.termux/files/usr/bin/youtube-dl
+        echo "Removing yt-dlp (using rm)"
+        rm -rf /data/data/com.termux/files/usr/bin/yt-dlp
         echo "Removal complete."
-        echo 'Python is still installed. If you need to remove Python, please do so by running "apt remove python".'
+        echo 'Dependencies may still be installed, please remove them manually.'
         exit
     else
         echo "Removal aborted"
