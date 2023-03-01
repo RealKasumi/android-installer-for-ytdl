46f2f3ab408e7dec4864ad075a0711af5474ba7c
RealKasumi <49617028+RealKasumi@users.noreply.github.com>
2023-03-01 13:32:50 +0000

    Update multi-install.sh

--- multi-install.sh
+++ multi-install.sh
@@ -8,11 +8,13 @@ else
     echo
 fi
 
-echo "yt-dlp installer for Termux-Android"
+echo "yt-dlp installer/manager for Termux-Android"
 echo
 echo "Select an option:"
-echo "1: Install yt-dlp"
-echo "2: Uninstall yt-dlp"
+echo "1: Install yt-dlp (stable)"
+echo "2: Install nyt-dlp (nightly yt-dlp)"
+echo "3: Uninstall yt-dlp"
+echo "4: Uninstall nyt-dlp"
 echo "Any other key: quit"
 echo
 echo -n "Select any option and hit ENTER: "
@@ -47,6 +49,31 @@ elif [[ $x -eq 2 ]]
 then
     echo 
     echo "The following processes are about to occur:"
+    echo "Install/Update dependency: Python (via apt)"
+    echo "Install/Update dependency: ffmpeg (via apt)"
+    echo "nyt-dlp is downloaded and installed to /data/data/com.termux/files/usr/bin"
+    echo "nyt-dlp is made executable"
+    echo
+    echo -n 'To continue, enter "y". To abort, press any other key: '
+    read y
+    if [[ $y == "y" ]] 
+    then
+        echo "Installing/updating dependencies (using apt)"
+        apt -y install python ffmpeg
+        echo "Installing nyt-dlp to /data/data/com.termux/files/usr/bin/nyt-dlp (using curl)"
+        curl -L https://github.com/ytdl-patched/ytdl-patched/releases/latest/download/ytdl-patched -o /data/data/com.termux/files/usr/bin/nyt-dlp
+        echo "Making nyt-dlp executable (using chmod)"
+        chmod a+rx /data/data/com.termux/files/usr/bin/nyt-dlp
+        echo "Install complete."
+        exit
+    else 
+        echo "Installation aborted"
+        exit
+    fi
+elif [[ $x -eq 3 ]] 
+then
+    echo 
+    echo "The following processes are about to occur:"
     echo "yt-dlp is removed from /data/data/com.termux/files/usr/bin"
     echo
     echo -n 'To continue, enter "y". To abort, press any other key: '
@@ -62,6 +89,26 @@ then
         echo "Removal aborted"
         exit
     fi
+elif [[ $x -eq 4 ]] 
+then
+    echo 
+    echo "The following processes are about to occur:"
+    echo "nyt-dlp is removed from /data/data/com.termux/files/usr/bin"
+    echo
+    echo -n 'To continue, enter "y". To abort, press any other key: '
+    read y
+    if [[ $y == "y" ]] 
+    then
+        echo "Removing nyt-dlp (using rm)"
+        rm -rf /data/data/com.termux/files/usr/bin/nyt-dlp
+        echo "Removal complete."
+        echo 'Dependencies may still be installed, please remove them manually.'
+        exit
+    else
+        echo "Removal aborted"
+        exit
+    fi
 else
+    echo "Installer terminated."
     exit
 fi
